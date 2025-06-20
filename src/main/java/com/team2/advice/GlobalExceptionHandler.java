6b26ba8895dd7aade33d99e544d5416ab211222d
bill291104 <bill29123@gmail.com>
2025-06-20 14:57:39 +0900

    feat(GlobalExceptionHandler.java): 404, 500 에러 처리
    잘못된 경로에 대한 요청은 404.jsp 로
    서버 내부에서 발생한 예외를 모두 잡아서 500.jsp 로 매핑

---
+++ src/main/java/com/team2/advice/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.team2.advice;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 404 처리 (존재하지 않는 URL 요청 시)
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFound(Exception ex, Model model) {
+        model.addAttribute("errorMessage", "페이지를 찾을 수 없습니다.");
+        return "error/404";
+    }
+
+    // 500 처리 (모든 예외)
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(Exception ex, Model model) {
+        System.out.println("handler called");
+        model.addAttribute("errorMessage", "서버 내부 오류가 발생했습니다.");
+        return "error/500";
+    }
+}
