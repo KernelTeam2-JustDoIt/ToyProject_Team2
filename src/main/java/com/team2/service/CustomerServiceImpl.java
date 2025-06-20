cf42a65886fb5ce82f9e91ae1e4e72914161034d
nghyeokSim <simjeonghyeok1020@gmail.com>
2025-06-20 18:51:16 +0900

    feat(CustomerServiceImpl.java): CustomerServiceImpl 추가

--- src/main/java/com/team2/service/CustomerServiceImpl.java
+++ src/main/java/com/team2/service/CustomerServiceImpl.java
@@ -1,8 +1,7 @@
-// src/main/java/com/team2/service/CustomerServiceImpl.java
-
 package com.team2.service;
 
 import com.team2.mapper.CustomerMapper;
+import com.team2.mapper.CustomerModel;
 import com.team2.model.CustomerVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,22 +9,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
-
     private static final Logger log = LoggerFactory.getLogger(CustomerServiceImpl.class);
     private final CustomerMapper customerMapper;
-
-    public CustomerServiceImpl(CustomerMapper customerMapper) {
-        this.customerMapper = customerMapper;
+    public CustomerServiceImpl(CustomerModel customerModel) {
+        this.customerModel = customerModel;
     }
 
     @Override
     public void registerCustomer(CustomerVO customer) {
         if (customer == null || customer.getCustomerLoginId() == null) {
-            log.warn("잘못된 고객 정보: {}", customer);
+            log.warn("잘못된 고객 정보: {}" , customer); //경고 로그
             throw new IllegalArgumentException("잘못된 고객 정보입니다.");
         }
 
-        log.info("회원가입 요청: {}", customer.getCustomerLoginId());
-        customerMapper.insertCustomer(customer);  // 여기서 MyBatis SQL 실행됨
+        log.info("회원가입 요청: {}", customer.getCustomerLoginId()); // 정보 로그
+        customerModel.insertCustomer(customer);
     }
 }
