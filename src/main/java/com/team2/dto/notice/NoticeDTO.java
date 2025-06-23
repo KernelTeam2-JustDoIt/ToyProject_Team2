cf80560032d571feab8cf3aac62f901f3108a009
sangmin <mfits603@gmail.com>
2025-06-23 12:32:02 +0900

    feat(NoticeDTO.java): 공지사항 DTO

---
+++ src/main/java/com/team2/dto/notice/NoticeDTO.java
@@ -0,0 +1,23 @@
+package com.team2.dto.notice;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class NoticeDTO {
+    private int noticeId;
+    private int writerAdminId; //작성자
+    private String title;
+    private String content;
+    private LocalDateTime postedAt;
+    private LocalDateTime updatedAt;
+    private int updatedBy; //수정자
+    private String noticeStatus; // 활성상태
+    private boolean isMain; // 메인페이지 상단공지 여부
+    private Integer priority; // 상단 공지 우선순위
+    private boolean isNeedPin;   // 상단고정 여부
+
+}
