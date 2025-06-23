c6672a1f2e9b55aeef393d1caa297be4b88f8ad2
sangmin <mfits603@gmail.com>
2025-06-23 12:31:03 +0900

    feat(NoticeService): 공지사항 서비스

---
+++ src/main/java/com/team2/service/NoticeService.java
@@ -0,0 +1,14 @@
+package com.team2.service;
+
+import com.team2.dto.notice.NoticeDTO;
+import java.util.List;
+
+public interface NoticeService {
+    NoticeDTO getNoticeDetail(int noticeId);
+    List<NoticeDTO> getNoticeList();
+    NoticeDTO getLatestNotice();
+    void insertNotice(NoticeDTO noticeDTO);
+    void updateNotice(NoticeDTO noticeDTO);
+    void deleteNotice(int noticeId);
+
+}
