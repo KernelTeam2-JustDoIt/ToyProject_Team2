32507c44a23442c31cab8eb1d78dd95f98c82c6b
sangmin <mfits603@gmail.com>
2025-06-23 18:44:45 +0900

    fix(NoticeMapper.  xml,java): 공지사항 페이징 처리위한 로직 추가

--- src/main/java/com/team2/mapper/NoticeMapper.java
+++ src/main/java/com/team2/mapper/NoticeMapper.java
@@ -1,6 +1,8 @@
 package com.team2.mapper;
 
 import com.team2.dto.notice.NoticeDTO;
+import org.apache.ibatis.annotations.Param;
+
 import java.util.List;
 
 public interface NoticeMapper {
@@ -8,11 +10,14 @@ public interface NoticeMapper {
     // 최신 공지 1건
     NoticeDTO getLatestNotice();
 
-    // 전체 공지 목록
-    List<NoticeDTO> getNoticeList();
+    // 공지 목록
+    List<NoticeDTO> getNoticeList(@Param("offset") int offset, @Param("pageSize") int pageSize);
 
     // 공지 상세 조회
     NoticeDTO getNoticeDetail(int noticeId);
+
+
+    int countNotices();
 
     void deleteNotice(int noticeId);
 
