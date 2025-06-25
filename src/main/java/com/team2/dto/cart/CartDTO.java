b5f9fede128572acce661339ba6e5b538ff933d0
yk <juyk.oung@gmail.com>
2025-06-25 21:35:01 +0900

    feat(CartDT)): 기능 수정

--- src/main/java/com/team2/dto/cart/CartDTO.java
+++ src/main/java/com/team2/dto/cart/CartDTO.java
@@ -3,27 +3,22 @@ package com.team2.dto.cart;
 import com.team2.model.Cart;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
 import java.util.Date;
 
 @Getter
 @Setter
+@ToString
 public class CartDTO {
     private int cartId;
-    private int roomId;
+    private long roomId;
     private int customerId;
-    private int adultCount;
-    private int childCount;
-    private Date desiredCheckInAt;
-    private Date desiredCheckOutAt;
+    private String checkIn;
+    private String checkOut;
+    private int adultCnt;
+    private int babyCnt;
+//    private Date desiredCheckInAt;
+//    private Date desiredCheckOutAt;
 
-    public CartDTO() { }
-
-    public CartDTO(int roomId, int customerId, int adultCount, int childCount, Date desiredCheckInAt, Date desiredCheckOutAt) {
-        this.roomId = roomId;
-        this.customerId = customerId;
-        this.adultCount = adultCount;
-        this.childCount = childCount;
-        this.desiredCheckInAt = desiredCheckInAt;
-        this.desiredCheckOutAt = desiredCheckOutAt;
-    }
 }
