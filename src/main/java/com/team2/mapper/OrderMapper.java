476ca09d1fb7fcf9bb6b961d783514673f1249a7
Park-HyeJeong <park061138@gmail.com>
2025-06-25 15:31:59 +0900

    feat: 주문 Mapper 인터페이스 추가

---
+++ src/main/java/com/team2/mapper/OrderMapper.java
@@ -0,0 +1,21 @@
+package com.team2.mapper;
+
+import com.team2.dto.order.OrderDTO;
+import com.team2.dto.order.OrderItemDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface OrderMapper {
+
+    int insertOrder(OrderDTO order);
+
+    int insertOrderItem(OrderItemDTO item);
+
+    List<OrderDTO> selectOrdersByCustomerId(int customerId);
+
+    OrderDTO selectOrderById(int orderId);
+
+    List<OrderItemDTO> selectOrderItems(int orderId);
+} 
