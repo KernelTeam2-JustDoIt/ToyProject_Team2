e91d7589fdd97e0ffcb811e5f752721a808c22d9
Park-HyeJeong <park061138@gmail.com>
2025-06-25 15:33:18 +0900

    feat: 주문 서비스 인터페이스 추가

---
+++ src/main/java/com/team2/service/OrderService.java
@@ -0,0 +1,27 @@
+package com.team2.service;
+
+import com.team2.dto.order.CartItemDTO;
+import com.team2.dto.order.OrderDTO;
+
+import java.util.List;
+
+public interface OrderService {
+
+    /**
+     * 장바구니 항목들을 기반으로 주문을 생성하고 결제를 완료 처리합니다.
+     * @param customerId 로그인 사용자 ID
+     * @param cartItems 결제할 장바구니 항목
+     * @return 생성된 주문 정보
+     */
+    OrderDTO createOrder(int customerId, List<CartItemDTO> cartItems);
+
+    /**
+     * 특정 사용자에 대한 주문(예약) 목록
+     */
+    List<OrderDTO> getOrdersByCustomer(int customerId);
+
+    /**
+     * 주문 상세 조회
+     */
+    OrderDTO getOrderDetail(int orderId);
+} 
