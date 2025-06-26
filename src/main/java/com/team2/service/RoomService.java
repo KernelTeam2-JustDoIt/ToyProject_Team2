9af55422d2cb62d36f6bbdad84e0f72efe240156
Park-HyeJeong <park061138@gmail.com>
2025-06-26 15:45:42 +0900

    feat: 객실 DTO,매퍼,Service 계층 추가

---
+++ src/main/java/com/team2/service/RoomService.java
@@ -0,0 +1,20 @@
+package com.team2.service;
+
+import com.team2.dto.RoomDTO;
+import java.util.List;
+
+/**
+ * 객실(Room) 조회 비즈니스 로직 인터페이스.
+ */
+public interface RoomService {
+
+    /**
+     * 판매가능한 모든 객실 목록
+     */
+    List<RoomDTO> getAllRooms();
+
+    /**
+     * 객실 단건 조회
+     */
+    RoomDTO getRoom(int roomId);
+} 
