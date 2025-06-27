5f7e2b4640644d9d1728880d7580dcc5cfbe3b86
sangmin <mfits603@gmail.com>
2025-06-27 09:55:03 +0900

    feat(QnaMapper): qna

---
+++ src/main/java/com/team2/mapper/QnaMapper.java
@@ -0,0 +1,31 @@
+package com.team2.mapper;
+
+import com.team2.dto.qna.QnaDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface QnaMapper {
+
+    //  고객 문의 작성
+    void insertQna(QnaDTO qna);
+
+    //  관리자 전체 QNA 목록 (페이징 + 검색 포함)
+    List<QnaDTO> getQnaList(Map<String, Object> param);
+
+    //  QNA 상세 조회
+    QnaDTO getQnaDetail(int qnaId);
+
+    //  관리자 답변 등록
+    void answerQna(QnaDTO qna);
+
+    //  QNA 총 개수 조회 (페이징용)
+    int countQnaList(Map<String, Object> param);
+
+    void softDelete(int qnaId);
+
+    void updateQna(QnaDTO qna);
+}
+
